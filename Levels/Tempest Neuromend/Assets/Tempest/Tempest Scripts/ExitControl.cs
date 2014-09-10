c54d35dbb456a41b924992550e028134b89c01eb
Ary <arachniblade@hotmail.com>
2014-09-10 22:49:04 +0800

    fix attempt part 2
    work?

---
+++ Levels/Tempest Neuromend/Assets/Tempest/Tempest Scripts/ExitControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitControl : MonoBehaviour 
+{
+
+	public GameObject theTarget;
+	public Vector3 exitOffset = new Vector3(0,5,0);
+	
+	void Start() 
+	{
+	}
+	
+	void Update() 
+	{
+		theTarget=GameObject.FindWithTag("Player");
+		transform.position = theTarget.transform.position+exitOffset;
+	}
+}
