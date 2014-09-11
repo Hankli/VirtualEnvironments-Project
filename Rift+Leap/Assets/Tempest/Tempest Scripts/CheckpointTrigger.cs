216faa5a0dc42e7160b6ea3c97c2531684155e3b
Hannah <hfklinac@hotmail.com>
2014-09-11 15:11:27 +0800

    Leap + Menu
    Uploaded what I have for Leap Motion and also a prototype menu

---
+++ Rift+Leap/Assets/Tempest/Tempest Scripts/CheckpointTrigger.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour 
+{
+
+	[Tooltip("The tag of the object intended to trigger this checkpoint")]
+	public string triggerObjectTag ="";//trigger object tag must be input in editor
+	[Tooltip("The overall level completion percentage this checkpoint represents (may change to precentage value rather than overall percentage...)")]
+	public float checkpointValue = 0.0f;//level complete percentage set in unity editor
+	
+	GameObject levelControl;
+	GameObject gameControl;
+	GameControl gameControlScript;
+	bool b_hasBeenTriggered=false;
+	
+		
+	void Start() 
+	{
+	}
+	
+	void Update() 
+	{
+	}
+	
+	void OnTriggerEnter(Collider theObject)
+	{
+		if(!b_hasBeenTriggered)
+		{
+			//need to check if correct object triggered it...
+			if(theObject.gameObject.tag==triggerObjectTag)
+			{
+				if(gameControl=GameObject.FindWithTag("Game"))
+				{
+					if(levelControl=GameObject.Find("Level Control"))
+					{
+						//maybe move this check somewhere else and have checkpoint values add up?
+						if(checkpointValue>=100)
+						{
+							levelControl.GetComponent<LevelControl>().LevelComplete(true);
+						}
+						
+						gameControlScript=gameControl.GetComponent<GameControl>();
+						string levelTag = levelControl.tag.ToString();
+						switch(levelTag)
+						{
+							case "ObjectInteraction":
+								gameControlScript.setOICheckpoint(checkpointValue);
+								break;				
+							case "ObjectAvoidance":
+								gameControlScript.setOACheckpoint(checkpointValue);				
+								break;
+							case "WayFinding":
+								gameControlScript.setWFCheckpoint(checkpointValue);				
+								break;
+						}
+					}
+				}
+				b_hasBeenTriggered=true;
+			}
+		}
+	}
+}
