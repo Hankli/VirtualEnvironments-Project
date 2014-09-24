37f4f13b37d99c5d9ac3ecebc26ffd0c5ba7e756
Hannah <hfklinac@hotmail.com>
2014-09-24 20:18:13 +0800

    Leap integrated

---
+++ Rift+Leap/Tempest Leap (Prototype)/Assets/LeapButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapButton : MonoBehaviour 
+{
+	private void OnTriggerEnter(Collider cl)
+	{
+		Debug.Log ("panel trigger");
+		TouchPanelButton button = cl.transform.GetComponent<TouchPanelButton> ();
+		
+		if(button)
+		{
+			button.OnClick();
+		}
+	}
+}
