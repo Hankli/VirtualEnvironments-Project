f684b1a2fdf895e2e3d24a69b9163844b1f93e35
Hannah <hfklinac@hotmail.com>
2014-09-26 20:55:59 +0800

    Leap prototype project with OVR instead of Cam set up

---
+++ Rift+Leap/Tempest Leap (Prototype) OVR/Assets/313_DatabaseFiles/SQLView.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Tempest
+{
+	namespace SQL
+	{
+		public class SQLView : MonoBehaviour
+		{
+			public static string m_defaultConfig = "Server=au-cdbr-azure-east-a.cloudapp.net;Database=cdb_de02ef7d99;User ID=bdf6053979c316;Password=eaadd23f;Pooling=false";
+			private string m_currentConfig = null;
+
+			private MySqlConnection m_connection = null;
+			private MySqlCommand m_command = null;
+			private MySqlDataReader m_reader = null;
+
+			private void Start()
+			{
+				OpenConnection (m_defaultConfig);
+
+
+				//SetQuery ("drop table patient");
+				//CommitQuery ();
+
+				//CREATING TABLES  NO TRIGGERS :( )
+
+				/*
+				BeginQuery ();
+
+					SetQuery("CREATE TABLE device(" +
+	                         "Name VARCHAR(50)," +
+							 "Description VARCHAR(100), " +
+							 "CONSTRAINT PRIMARY KEY(Name))");
+					CommitQuery ();
+	
+
+					SetQuery("CREATE TABLE activity(" +
+					         "Name VARCHAR(50)," +
+					         "Description VARCHAR(100)," +
+					         "CONSTRAINT PRIMARY KEY(Name))");
+					CommitQuery ();
+
+
+					SetQuery ("CREATE TABLE patient(" +
+						"PatientID INT NOT NULL AUTO_INCREMENT, " +
+					    "Gender VARCHAR(10), " +
+						"DateOfBirth DATE, " +
+						"MedicalCondition VARCHAR(100), " +
+						"CONSTRAINT PRIMARY KEY(PatientID))");	   
+					CommitQuery ();
+
+
+					SetQuery("CREATE TABLE activityreport(" +
+					         "ReportID INT NOT NULL AUTO_INCREMENT," +
+					         "PatientID INT NOT NULL," +
+					         "DeviceName VARCHAR(50) NOT NULL," +
+					         "ActivityName VARCHAR(50) NOT NULL," +
+					         "CompletionDate DATE NOT NULL," +
+					         "CompletionScore INT NOT NULL," +
+					         "PRIMARY KEY(ReportID)," +
+					         "FOREIGN KEY(DeviceName) REFERENCES device(Name) ON UPDATE CASCADE ON DELETE RESTRICT, " +
+					         "FOREIGN KEY(ActivityName) REFERENCES activity(Name) ON UPDATE CASCADE ON DELETE RESTRICT, " +
+					         "FOREIGN KEY(PatientID) REFERENCES patient(PatientID) ON UPDATE CASCADE ON DELETE RESTRICT)" );
+					CommitQuery();
+
+
+					SetQuery ("CREATE TABLE patientaccount(" +
+							  "Username VARCHAR(20) UNIQUE NOT NULL, " +
+							  "Password VARCHAR(20) NOT NULL, "+
+				          	  "PatientID INT NOT NULL, " +
+				              "PRIMARY KEY(Username))," +
+				              "FOREIGN KEY REFERENCES patient(PatientID) ON UPDATE CASCADE ON DELETE RESTRICT"); 
+				    CommitQuery();
+
+
+				    EndQuery();
+
+					//*/
+
+				/* ADDING DATA
+		        BeginQuery();
+				SetQuery("insert into patient(DateOfBirth, Gender, Description, Height, Weight) values (?DOB, ?GEND, ?DESC, ?HGT, ?WGT)");
+				Write ("?DOB", MySqlDbType.Date, System.Convert.ToDateTime("5/3/1991"));
+				Write ("?GEND", MySqlDbType.VarChar, "Female");
+				Write ("?DESC", MySqlDbType.VarChar, "Severe stroke.");
+				Write ("?HGT", MySqlDbType.Int32, 166);
+				Write ("?WGT", MySqlDbType.Int32, 67);
+				CommitQuery ();
+
+				SetQuery("insert into patient(DateOfBirth, Gender, Description, Height, Weight) values (?DOB, ?GEND, ?DESC, ?HGT, ?WGT)");
+				Write ("?DOB", MySqlDbType.Date, System.Convert.ToDateTime("12/7/1961"));
+				Write ("?GEND", MySqlDbType.VarChar, "Male");
+				Write ("?DESC", MySqlDbType.VarChar, "Loss of motor functionality in hands.");
+				Write ("?HGT", MySqlDbType.Int32, 189);
+				Write ("?WGT", MySqlDbType.Int32, 79);
+				CommitQuery ();
+				EndQuery();
+
+**/
+				BeginQuery ("select * from patient");
+				CommitQuery ();
+
+				MySqlDataReader rdr = null;
+				while( (rdr = Read ()) != null)
+				{
+					Debug.Log (rdr["PatientID"].ToString());
+					Debug.Log (rdr["Gender"].ToString());
+					Debug.Log (rdr["DateOfBirth"].ToString());
+					Debug.Log (rdr["Description"].ToString());
+				}
+				EndQuery ();
+
+
+
+				CloseConnection ();
+			}
+
+			
+			public void OpenConnection(string config)
+			{
+				m_currentConfig = config;
+				m_connection = new MySqlConnection (m_currentConfig);
+				m_connection.Open ();
+			}
+			
+			public void CloseConnection()
+			{
+				//MySqlConnection.ClearPool (m_connection);
+				m_connection.Dispose();
+				m_connection.Close ();					 
+			}
+
+			public bool OpenConnectionState
+			{
+				get { return m_connection.State == System.Data.ConnectionState.Open; }
+			}
+
+			public bool ClosedConnectionState
+			{
+				get { return m_connection.State == System.Data.ConnectionState.Closed; }
+			}
+
+			public void BeginQuery(string text = null)
+			{
+				if(m_command == null)
+				{
+					m_command = new MySqlCommand (text, m_connection);
+				}
+			}
+			
+			public void CommitQuery()
+			{
+				if(m_command != null)
+				{
+					try
+					{
+						m_command.ExecuteNonQuery ();
+					}
+					catch(MySqlException ex)
+					{
+						Debug.Log (ex.Message);
+					}
+				}
+			}
+			
+			public void EndQuery()
+			{
+				if(m_command != null)
+				{
+					m_command.Dispose ();
+					m_command = null;
+				}
+			}
+
+			public void SetQuery (string query)
+			{
+				if(m_command != null)
+				{
+					m_command.CommandText = query;
+				}
+			}
+
+			public string GetQueryResult()
+			{
+				string txt = null;
+				MySqlDataReader rdr;
+
+				while((rdr = Read ()) != null)
+				{
+					for(int i=0; i<rdr.FieldCount; i++)
+					{
+						txt += rdr.GetValue(i).ToString();
+						txt += " ";
+					}
+					txt += "\n";
+				}
+				return txt;
+			}
+
+			public void Write(string param, MySqlDbType sqlType, object value)
+			{
+				if(m_command != null)
+				{
+					MySqlParameter sqlParam = m_command.Parameters.Add (param, sqlType);
+					sqlParam.Value = value;
+				}
+			}
+
+			public MySqlDataReader Read()
+			{
+				if(m_command == null)
+				{
+					return null;
+				}
+
+				if(m_reader == null)
+				{
+					m_reader = m_command.ExecuteReader();
+				}
+
+				if(m_reader.HasRows && m_reader.Read ())
+				{
+					return m_reader;
+				}
+
+				m_reader.Close();
+				m_reader = null;
+
+				return m_reader;
+			}
+		}
+	}
+}
+
---
+++ Rift+Leap/Tempest Leap (Prototype) OVR/Assets/LeapOculusMove.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapOculusMove : MonoBehaviour 
+{
+	Controller m_leapController;
+	float speed;
+	float forwardSpeed;
+	float backwardSpeed;
+	float sensitivity;
+	float rotSpeed;
+	float stopping;
+	Vector3 newPos;
+	Vector3 newRot;
+	OVRDevice oculus;
+	bool twoHands;
+
+	private CharacterController control;
+	
+	private LeapControl variables = null;
+	private GameObject gameControlObject = null;
+	
+	void Awake()
+	{
+		if(gameControlObject = GameObject.FindGameObjectWithTag("Game"))
+		{
+			if(variables=gameControlObject.GetComponent<LeapControl>())
+			{
+				//bleh
+			}
+		}
+		
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		forwardSpeed = 3.0f;
+		backwardSpeed = -2.0f;
+
+		//********variables changed in menu********//
+		twoHands = variables.GetTwoHands();//if two or 1 hands selected
+		sensitivity = variables.GetSensitivity();
+		Debug.Log ("sensitivity = " + sensitivity);
+		float OldRange = (10.0f - 0.0f);  
+		float NewRange = (3.5f - 2.5f);  
+		sensitivity = (((sensitivity - 0.0f) * NewRange) / OldRange) + 2.5f;
+		Debug.Log ("sensitivity = " + sensitivity);
+		//sensitivity = 2.9f; //sensitivity of the hand rotation. decrease for harder (2.5), increase for easier (3.1), ave 2.9.
+		//********variables changed in menu********//
+
+		speed = 2.0f;
+		rotSpeed = 0.5f;
+		control = transform.parent.GetComponent<CharacterController>();
+		oculus = GetComponent<OVRDevice> ();
+		
+		m_leapController = new Controller();
+		stopping = 0.0f;
+		if (transform.parent == null)
+		{
+			Debug.LogError("LeapMove must have a parent object to control (eg camera)");
+		}
+		
+	}
+	
+	Hand GetLeftMostHand(Frame f)
+	{
+		float smallestVal = float.MaxValue;
+		Hand h = null;
+		for (int i = 0; i < f.Hands.Count; ++i)
+		{
+			if (f.Hands[i].PalmPosition.ToUnity().x < smallestVal)
+			{
+				smallestVal = f.Hands[i].PalmPosition.ToUnity().x;
+				h = f.Hands[i];
+			}
+		}
+		return h;
+	}
+	
+	Hand GetRightMostHand(Frame f)
+	{
+		float largestVal = -float.MaxValue;
+		Hand h = null;
+		for (int i = 0; i < f.Hands.Count; ++i)
+		{
+			if (f.Hands[i].PalmPosition.ToUnity().x > largestVal)
+			{
+				largestVal = f.Hands[i].PalmPosition.ToUnity().x;
+				h = f.Hands[i];
+			}
+		}
+		return h;
+	}
+	
+	int isExtended(Hand hand)
+	{
+		int extendedFingers = 0;
+		for (int f = 0; f < hand.Fingers.Count; f++)
+		{
+			Finger digit = hand.Fingers[f];
+			if(digit.IsExtended) extendedFingers++;
+		}
+		return extendedFingers;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Frame frame = m_leapController.Frame();
+		Frame lastFrame = m_leapController.Frame(1);
+
+		HandList hands = frame.Hands;
+		Hand firstHand = hands[0];
+
+		float pitch = firstHand.Direction.Pitch;
+		float yaw = firstHand.Direction.Yaw;
+		//float roll = firstHand.PalmNormal.Roll;
+		Vector position = firstHand.PalmPosition;
+		Vector velocity = firstHand.PalmVelocity;
+		Vector direction = firstHand.Direction;
+		Vector normal = firstHand.PalmNormal;
+		
+		//The change of position of this hand between the current frame and the specified frame.
+		Vector handMovement = (firstHand.Translation(lastFrame));
+
+		newRot = transform.parent.localRotation.eulerAngles;
+
+		if(twoHands)
+		{
+			if (frame.Hands.Count == 1) 
+			{
+				//Vector handXBasis =  firstHand.PalmNormal.Cross(firstHand.Direction).Normalized;
+				float roll = firstHand.PalmNormal.Roll;
+				if((roll <= sensitivity) && (roll >= -sensitivity))
+					newRot.y -= roll; // * distance from center.
+				transform.parent.localRotation = Quaternion.Slerp(transform.parent.localRotation, Quaternion.Euler(newRot), rotSpeed);
+			}
+			else if (frame.Hands.Count >= 2)
+			{
+				stopping = 5.0f;//change for time it takes to stop after hands leave
+				Hand leftHand = GetLeftMostHand(frame);
+				Hand rightHand = GetRightMostHand(frame);
+
+				float rollLeft = leftHand.PalmNormal.Roll;
+				if((rollLeft <= sensitivity) && (rollLeft >= -sensitivity))
+					newRot.y -= rollLeft; // * distance from center.
+
+				transform.parent.localRotation = Quaternion.Slerp(transform.parent.localRotation, Quaternion.Euler(newRot), rotSpeed);
+
+				if ((isExtended (leftHand) > 3)&&(isExtended (rightHand) > 3))//if open hands
+				{
+					speed = forwardSpeed;
+				}
+				else if ((isExtended (leftHand) < 3)&&(isExtended (rightHand) < 3))//if closed fist
+				{
+					speed = backwardSpeed;
+				}
+
+				newPos = new Vector3((transform.parent.forward.x * speed), (transform.parent.forward.y * speed), (transform.parent.forward.z * speed));
+				control.SimpleMove (newPos);
+			}
+			else if(frame.Hands.Count == 0)
+			{
+				stopping--;
+				if(stopping==0.0f)
+				{
+					control.SimpleMove(newPos*0.0f);
+				}
+			}
+		}
+		else if(!twoHands)
+		{
+			if (frame.Hands.Count == 1) 
+			{
+				float roll = firstHand.PalmNormal.Roll;
+				if((roll <= sensitivity) && (roll >= -sensitivity))
+					newRot.y -= roll;
+				transform.parent.localRotation = Quaternion.Slerp(transform.parent.localRotation, Quaternion.Euler(newRot), rotSpeed);
+
+				if (isExtended (firstHand) > 3)//if open hands
+				{
+					speed = forwardSpeed;
+				}
+				else if (isExtended (firstHand) < 3)//if closed fist
+				{
+					speed = backwardSpeed;
+				}
+				
+				newPos = new Vector3((transform.parent.forward.x * speed), (transform.parent.forward.y * speed), (transform.parent.forward.z * speed));
+				control.SimpleMove (newPos);
+			}
+		}
+	}
+}
