3565619a9ea1c7a01f703f0f87eae33c77a1a3fd
Hannah <hfklinac@hotmail.com>
2014-09-26 19:34:03 +0800

    Moved old project

---
+++ Rift+Leap/Old/Tempest Leap (Prototype)/Assets/Tempest/Tempest Scripts/WindowTrigger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowTrigger : MonoBehaviour 
+{
+
+	public enum WindowType
+	{
+		Circle,
+		Square,
+		Triangle
+	};
+
+	public WindowType windowType;
+	private ThrowingObjective objectiveScript=null;
+	
+	void Awake() 
+	{
+		if(transform.parent.GetComponent<ThrowingObjective>())
+		{
+			objectiveScript = transform.parent.GetComponent<ThrowingObjective>();
+		}
+	}
+	
+	void Start() 
+	{
+	}
+	
+	void Update() 
+	{
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.GetComponent<ThrowableObject>())
+		{
+			
+			ThrowableObject thrownObjectScript=other.gameObject.GetComponent<ThrowableObject>();
+			if(thrownObjectScript.GetWindowType()==windowType)
+			{
+				objectiveScript.AdjustScore(1);
+			}
+			else
+			{
+				objectiveScript.AdjustErrors(1);
+			}
+		}
+	}
+}
