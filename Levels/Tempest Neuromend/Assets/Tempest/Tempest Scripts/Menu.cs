aa32b67d197a9f1042eacb20ca8e3e3baacfe8a6
Ary <arachniblade@hotmail.com>
2014-10-02 17:07:05 +0800

    Scene organisation
    Cleaned up scenes with 'folders'... note: if a game object is set to
    DontDestroyOnLoad() it will still be destroyed on load if it is a child
    of an object which is destroyed on load. So make sure the Game control
    object in Neuromend First is never a child of a destructable.

--- Levels/Tempest Neuromend/Assets/Tempest/Tempest Scripts/Menu.cs
+++ Levels/Tempest Neuromend/Assets/Tempest/Tempest Scripts/Menu.cs
@@ -5,55 +5,81 @@ public class Menu : MonoBehaviour
 {
 	Color backgroundColour = new Color (1.0f, 1.0f, 1.0f);
 	Color buttonColour = new Color (0.22f, 1.0f, 0.97f);
-	//Color textColour = new Color (1.0f, 1.0f, 1.0f);
-
+	Color textColour = new Color (1.0f, 1.0f, 1.0f);
+	
 	private delegate void MenuDelegate();
 	private MenuDelegate menuFunction;
-
+	
 	private float screenHeight;
 	private float screenWidth;
 	private float buttonHeight;
 	private float buttonWidth;
-	
-	public string firstLevel="OI Video Tutorial";
 
+	//private LeapControl variables = null;
+	private GameObject gameControlObject = null;
+
+	
+	bool sound;
+	float volume;
+	bool twoHands;
+	float sensitivity;
+
+	void Awake()
+	{
+		/*
+		if(gameControlObject = GameObject.FindGameObjectWithTag("Game"))
+		{
+			if(variables=gameControlObject.GetComponent<LeapControl>())
+			{
+				//bleh
+			}
+		}
+		*/
+	}
 	// Use this for initialization
 	void Start () 
 	{
+		sound = true;
+		volume = 5.0f;
+		twoHands = false;
+		sensitivity = 5.0f;
+		
 		screenHeight = Screen.height;
 		screenWidth = Screen.width;
-
+		
 		buttonHeight = Screen.height * 0.05f;
 		buttonWidth = Screen.width * 0.2f;
-
+		
 		Camera.main.backgroundColor = backgroundColour;
 		menuFunction = anyKey;
 	}
-
+	
 	void OnGUI()
 	{
 		menuFunction();
 	}
-
+	
 	void anyKey()
 	{
 		if(Input.anyKey) //if the user has pressed any key
 		{
 			menuFunction = mainMenu; //change the menu to main menu
 		}
-
+		
 		GUI.color = buttonColour;
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
 		GUI.Label(new Rect(screenWidth * 0.35f, screenHeight * 0.3f, screenWidth * 0.3f, screenHeight * 0.1f), "Press any key to continue");
-
+		
 	}
-
+	
 	void mainMenu()
 	{
 		GUI.color = buttonColour;
 		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), "PLAY"))
 		{
-			Application.LoadLevel (firstLevel);
+			//variables.SetTwoHands(twoHands);
+			//variables.SetSensitivity(sensitivity);
+			Application.LoadLevel ("Way Finding");
 		}
 		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.2f, buttonWidth, buttonHeight), "PROFILE"))
 		{
@@ -72,12 +98,12 @@ public class Menu : MonoBehaviour
 			Application.Quit ();
 		}
 	}
-
+	
 	void profile()
 	{
 		GUI.color = buttonColour;
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
-
+		
 		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), "LOAD"))
 		{
 			menuFunction = loadProfile;
@@ -91,17 +117,21 @@ public class Menu : MonoBehaviour
 			menuFunction = mainMenu;
 		}
 	}
-
+	
 	void settings()
 	{
 		GUI.color = buttonColour;
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
 
-		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), "CONTROLS"))
+		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), "CONFIGURATION"))
+		{
+			menuFunction = configuration;
+		}
+		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.2f, buttonWidth, buttonHeight), "CONTROLS"))
 		{
 			menuFunction = controls;
 		}
-		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.2f, buttonWidth, buttonHeight), "AUDIO"))
+		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.3f, buttonWidth, buttonHeight), "AUDIO"))
 		{
 			menuFunction = audio;
 		}
@@ -111,12 +141,42 @@ public class Menu : MonoBehaviour
 		}
 	}
 
-	new void audio()
+	void configuration()
+	{
+		float min = 0.0f;
+		float max = 10.0f;
+		GUI.color = buttonColour;
+
+		GUILayout.BeginArea(new Rect((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.2f, buttonWidth, buttonHeight*2.0f));
+		GUILayout.Label("Sensitivity");
+		sensitivity = GUILayout.HorizontalSlider(sensitivity, min, max);
+			GUILayout.BeginHorizontal();
+				var defaultAlignment = GUI.skin.label.alignment;
+				GUI.skin.label.alignment = TextAnchor.UpperLeft;
+				GUILayout.Label(min.ToString());
+				GUI.skin.label.alignment = TextAnchor.UpperRight;
+				GUILayout.Label(max.ToString());
+				GUI.skin.label.alignment = defaultAlignment;
+				GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+
+		twoHands = GUI.Toggle (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), twoHands, "Two hands");
+
+		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.8f, screenHeight * 0.8f, buttonWidth, buttonHeight), "BACK"))
+		{
+			menuFunction = settings;
+		}
+	}
+	
+	void audio()
 	{
 		GUI.color = buttonColour;
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
 		GUI.Label(new Rect(screenWidth * 0.45f, screenHeight * 0.3f, screenWidth * 0.1f, screenHeight * 0.1f), "*display audio options here*");
 		
+		sound = GUI.Toggle (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.1f, buttonWidth, buttonHeight), sound, "Sound");
+		GUI.Label(new Rect((screenWidth - buttonWidth) * 0.1f, screenHeight * 0.1f, buttonWidth, screenHeight), "Volume");
+		volume = GUI.HorizontalSlider (new Rect ((screenWidth - buttonWidth) * 0.2f, screenHeight * 0.2f, buttonWidth, buttonHeight), volume,0.0f,10.0f);
 		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.8f, screenHeight * 0.8f, buttonWidth, buttonHeight), "BACK"))
 		{
 			menuFunction = settings;
@@ -134,7 +194,7 @@ public class Menu : MonoBehaviour
 			menuFunction = mainMenu;
 		}
 	}
-
+	
 	void scores()
 	{
 		GUI.color = buttonColour;
@@ -146,13 +206,13 @@ public class Menu : MonoBehaviour
 			menuFunction = profile;
 		}
 	}
-
+	
 	void controls()
 	{
 		GUI.color = buttonColour;
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
 		GUI.Label(new Rect(screenWidth * 0.45f, screenHeight * 0.3f, screenWidth * 0.1f, screenHeight * 0.1f), "*insert controls here*");
-
+		
 		if(GUI.Button (new Rect ((screenWidth - buttonWidth) * 0.8f, screenHeight * 0.8f, buttonWidth, buttonHeight), "BACK"))
 		{
 			menuFunction = settings;
@@ -169,12 +229,12 @@ public class Menu : MonoBehaviour
 			menuFunction = profile;
 		}
 	}
-
-
-
+	
+	
+	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		
 	}
 }
