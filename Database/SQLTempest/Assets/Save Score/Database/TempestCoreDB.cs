6cb1defb348784f3326eab95ddc821cd86e0460f
tpvlyrm <bryan-500088@hotmail.com>
2014-10-14 23:12:56 +0800

    d

--- Database/SQLTempest/Assets/Save Score/Database/TempestCoreDB.cs
+++ Database/SQLTempest/Assets/Save Score/Database/TempestCoreDB.cs
@@ -23,12 +23,20 @@ namespace Tempest
 
 		public class TempestCoreDB : MonoBehaviour
 		{
+
 			public const string DefaultConfigIni = "Server=au-cdbr-azure-east-a.cloudapp.net;" +
 												   "Database=cdb_de02ef7d99;" +
 												   "User ID=bdf6053979c316;" +
 												   "Password=eaadd23f;" +
 												   "Pooling=false";
 
+			/*
+			public const string DefaultConfigIni = "Server=mysql.2freehosting.com;" +
+				"Database=u468443034_313;" +
+					"User ID=u468443034_313;" +
+					"Password=golaget123;" +
+					"Pooling=false";
+*/
 			private SQLView m_sqlSource = new SQLView();
 			private ActivityDB m_activityDB = null;
 			private DeviceDB m_deviceDB = null;
@@ -65,7 +73,7 @@ namespace Tempest
 
 			private void InitialiseDeviceData()
 			{
-				m_deviceDB.AddDevice ("Razor Hydra", "Motion and orientation detection game controller by Sixense Entertainment");
+				m_deviceDB.AddDevice ("Razer Hydra", "Motion and orientation detection game controller by Sixense Entertainment");
 				m_deviceDB.AddDevice ("Microsoft Kinect", "Motion sensing input device by Microsoft");
 				m_deviceDB.AddDevice ("Leap Motion", "Computer hardware sensor device that supports hand and finger motions as input");
 			}
@@ -75,11 +83,12 @@ namespace Tempest
 				m_activityDB.AddActivity ("Object Avoidance", "Tasks that involve navigating around, dodging and avoding obstacles in general");
 				m_activityDB.AddActivity ("Object Interaction", "Tasks that involve manipulating of objects such as picking, pushing and throwing"); 
 				m_activityDB.AddActivity ("Wayfinding", "Tasks that involve pathfinding related puzzles");
+
 			}
 
-			public void Reconnect(string config)
+			public bool Reconnect(string config)
 			{
-				if(config.Length == 0) return;
+				if(config.Length == 0) return false;
 
 				m_sqlSource.OpenConnection(config);
 				
@@ -102,7 +111,9 @@ namespace Tempest
 				    InitialiseActivityData ();
 				
 					m_profile = new Profile();
+					return true;
 				}
+				return false;
 			}
 
 			public bool IsConnected
@@ -118,33 +129,30 @@ namespace Tempest
 
 			private void Start()
 			{
-				m_profile = new Profile ();
-			
 				DontDestroyOnLoad (gameObject);
-				Reconnect (DefaultConfigIni);
+				if(Reconnect (DefaultConfigIni))
+				{
+					//keep for testing purposes ONLY
+					m_reportDB.DropRelation ();
+					m_deviceDB.DropRelation ();
+					m_activityDB.DropRelation ();
+					m_patientDB.DropRelation ();
 
+					m_deviceDB.CreateRelation ();
+					m_patientDB.CreateRelation ();
+					m_activityDB.CreateRelation ();
+					m_reportDB.CreateRelation ();
 
-				//keep for testing purposes ONLY
-				m_reportDB.DropRelation ();
-				m_deviceDB.DropRelation ();
-				m_activityDB.DropRelation ();
-				m_patientDB.DropRelation ();
+					InitialiseActivityData();
+					InitialiseDeviceData();
 
-				m_deviceDB.CreateRelation ();
-				m_patientDB.CreateRelation ();
-				m_activityDB.CreateRelation ();
-				m_reportDB.CreateRelation ();
+					m_patientDB.AddPatient ("Bryan", "password", "12/5/1209", "Male", "Bad");
 
-
-				m_patientDB.AddPatient ("Hello", "password", "12/5/1209", "Male", "Bad");
-				m_deviceDB.AddDevice ("xbox controller", "n X-BOX controller");
-				m_activityDB.AddActivity ("swimming", "Swim N Drown");
-				m_activityDB.AddActivity ("flying", "Fly");
-
-				List<ReportDB.Report> list = new List<ReportDB.Report> ();
-				m_reportDB.AddReport ("Hello", "xbox controller", "swimming", System.DateTime.Now, 120);
-				m_reportDB.AddReport ("Hello", "xbox controller", "swimming", System.DateTime.Now, 156);
-				m_reportDB.AddReport ("Hello", "xbox controller", "flying", System.DateTime.Now, 1999);
+					List<ReportDB.Report> list = new List<ReportDB.Report> ();
+					m_reportDB.AddReport ("Bryan", "Microsoft Kinect", "Object Avoidance", System.DateTime.Now, 120);
+					m_reportDB.AddReport ("Bryan", "Razer Hydra", "Object Interaction", System.DateTime.Now, 156);
+					m_reportDB.AddReport ("Bryan", "Leap Motion", "Wayfinding", System.DateTime.Now, 1999);
+				}
 			}
 
 			private void OnApplicationQuit()
